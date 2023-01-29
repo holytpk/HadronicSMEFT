9f13a3fd92c2e3be4230aed4098c9c2c6c3b17b7
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-01-29 10:52:46 +0100

    had

--- postprocessing/genPostProcessing.sh
+++ postprocessing/genPostProcessing.sh
@@ -1,2 +1,5 @@
 #python genPostProcessing.py --addReweights --targetDir v8 --sample TT01jDebug --delphesEra ATLAS --removeDelphesFiles #SPLIT199 
 python genPostProcessing.py --addReweights --targetDir v8 --sample TT01j1l --delphesEra ATLAS --removeDelphesFiles #SPLIT200 
+
+python genPostProcessing.py --addReweights --targetDir v8 --sample TT01j2l --delphesEra ATLAS --removeDelphesFiles #SPLIT200 
+python genPostProcessing.py --addReweights --targetDir v8 --sample TT01j2l_HT800 --delphesEra ATLAS --removeDelphesFiles #SPLIT200 
